bb775215b1e94ad16b811a68af6d158b7c636b9f
MathPSantos <matheus.psantos2016@gmail.com>
2023-01-15 17:34:23 -0300

    feat: add sora font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
+import { Sora } from "@next/font/google";
 
 import { queryClient } from "@libs/query";
 
 import "@styles/main.css";
 
+const soraFont = Sora({
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
+      <style jsx global>{`
+        :root {
+          --sora-font: ${soraFont.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </QueryClientProvider>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--sora-font)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
