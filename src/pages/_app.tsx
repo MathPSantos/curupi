9951e6109d26ca8e31b2d2cff341d7448a51ac13
MathPSantos <matheus.psantos2016@gmail.com>
2023-01-15 17:22:53 -0300

    chore: add @tanstack/react-query package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
+import { queryClient } from "@libs/query";
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
